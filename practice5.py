15c9c1a7f5b7f8d0d604004eb664b463e58f9b28
“SOLIDCHAIR” <Stephen.d3nham@gmail.com>
2024-10-04 14:58:09 -0400

    created

---
+++ practice5.py
@@ -0,0 +1,19 @@
+def pattern_creator(number):
+    result = ""
+    next_character = "1"
+    for i in range(1, number+1):
+        if (next_character == "1"):
+            result = result + next_character
+            next_character = "0"
+        else:
+            result = result + next_character
+            next_character = "1"
+    return (result)
+
+userin = int(input("Enter your number:"))
+
+output = pattern_creator(userin)
+print(output)
+    
+
+        
