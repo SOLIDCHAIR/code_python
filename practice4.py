1c6da838d26c90bc75226ed6a2e27732cd255582
“SOLIDCHAIR” <Stephen.d3nham@gmail.com>
2024-10-04 12:38:39 -0400

    created

---
+++ practice4.py
@@ -0,0 +1,12 @@
+def constant_checker(word):
+    lower_word = word.lower()
+    counter = 0
+    for character in (lower_word):
+        if character.isalpha() and character not in ("a", "e", "i", "o", "u"):
+            counter = counter + 1
+    return (counter)
+
+characters = (input("Enter your string:"))
+
+output = constant_checker(characters)
+print(output)
